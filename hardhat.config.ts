9f18c3d4a0a502fa88842b8b987ab591e39d1a02
unknown <adamkhatib01@gmail.com>
2026-04-09 10:02:30 +0200

    config sepolia + deploiement SimpleStorage

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,19 +1,32 @@
-import { defineConfig } from "hardhat/config";
+import "dotenv/config";
+import { defineConfig, configVariable } from "hardhat/config";
 import HardhatToolboxMochaEthers from "@nomicfoundation/hardhat-toolbox-mocha-ethers";
+
+const SEPOLIA_RPC_URL = configVariable("SEPOLIA_RPC_URL");
+const PRIVATE_KEY = configVariable("PRIVATE_KEY");
+const ETHERSCAN_API_KEY = configVariable("ETHERSCAN_API_KEY");
 
 export default defineConfig({
   solidity: {
     version: "0.8.28",
     settings: {
-      optimizer: {
-        enabled: true,
-        runs: 200,
-      },
+      optimizer: { enabled: true, runs: 200 },
     },
   },
   networks: {
     hardhat: {
       type: "edr-simulated",
+    },
+    sepolia: {
+      type: "http",
+      url: SEPOLIA_RPC_URL,
+      accounts: [PRIVATE_KEY],
+      chainId: 11155111,
+    },
+  },
+  verify: {
+    etherscan: {
+      apiKey: ETHERSCAN_API_KEY,
     },
   },
   plugins: [HardhatToolboxMochaEthers],
